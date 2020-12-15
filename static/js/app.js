73f7e5e2c655dd1cff97a3db4b2b4663b1ee922f
JeniLam <jenilam639@gmail.com>
2020-12-14 22:08:01 -0600

    Adds some css to dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -2,15 +2,11 @@
 // need python -m http.server to load in data locally
 // http://localhost:8000/index.html
 // get data and console.log it
-d3.json('samples.json').then((samples => console.log(samples)));
+d3.json('samples.json').then((data => console.log(data)));
 // above would look like below if not in arrow function
 //  function(samples) {
 //      console.log(samples)
 //  };
-// Once data is loaded will need to create variable for loaded data done is above as sample
-
-
-
 
 // initialize the page with default charts Day 2 Activity 8,9 - use subject 940 to populate
 // then add data to drop down so user can dynamically adjust the charts Day 2 activity 7  
@@ -18,11 +14,11 @@ d3.json('samples.json').then((samples => console.log(samples)));
 function init(){
     var selector = d3.select("selDataset");
         //Grab values from samples.json 
-        d3.json("samples.json").then((sample) => {
-            var sampleIDNames = sample.names;
+        d3.json("samples.json").then((data) => {
+            var sampleIDNames = data.names;
             console.log("IDs ", sampleIDNames);
-            sampleIDNames.forEach((entry) => {
-                selector.append("option").text(entry).property("value");
+            sampleIDNames.forEach((sample) => {
+                selector.append("option").text(sample).property("value");
             })
             //build chart with first id name
             buildPlot(sampleIDNames[0]);
@@ -30,13 +26,49 @@ function init(){
     };
 
 init();
+// still need to get the array into the dropdown and initialize charts with ID 940
+
+//Event listener function #2
+function optionChanged(newSample) {
+    //recall function inside option change (buildPlot(neewsample))
+        buildPlot(newSample);
+    };
+
 
 // Step 2 - Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use sample_values as the values for the bar chart
 // Use otu_ids as labels for the bar chart
 // Use otu_labels as the hovertext for the chart
-
-
+function buildPlot(sample) {
+    d3.json('samples.json').then((data => {
+    //Filter function matching the id
+        var samples = data.samples 
+        var filteredData = samples.filter(sampleID => sampleID.id === samples[0])
+        console.log(filteredData);
+        //Use sample_values as the values for the bar chart.
+        var sample_values = filteredData.sample_values;
+        console.log(sample_values);
+        //Use otu_ids as the labels for the bar chart.
+        var otu_ids = filteredData.otu_ids;
+        console.log(otu_ids);
+        //Use otu_labels as the hovertext for the chart.
+        var otu_labels = filteredData.otu_labels;
+        console.log(otu_labels);
+        //trace, layout go here *****USE SLICE & REVERSE to display top 10 OTUs
+        let trace1 = {
+            x: sample_values,
+            y: otu_ids,
+            text: otu_labels,
+            type: 'bar',
+            orientation: 'h'
+        };
+        let layout = {
+            xaxis: { title: "Sample Values" },
+            yaxis: { title: "OTU IDs" }
+        };
+        Plotly.newPlot("bar",trace1, layout);
+    })); 
+};
 
 
 // Step 3 - Create a bubble chart that displays each sample.
