d79fdf3d70ff2a5d80b3d7be4d53a15b364d97b6
JeniLam <jenilam639@gmail.com>
2020-12-17 23:05:42 -0600

    Adds restarting app to take step by step

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,25 @@
-// Step 1 - Use the D3 library to read in samples.json. Day 3 activity 6
+// Use the D3 library to read in samples.json. and use to build bar plot
 // need python -m http.server to load in data locally
 // http://localhost:8000/index.html
-// get data and console.log it - use data variable throughout the code. 
-d3.json('samples.json').then((data => {
-    console.log(data);
 
-    var data = data;
-    // use d3 to add test subject ID to drop down (need to loop through using forEach to get all in)
-    var names = data.names;
-    // https://alignedleft.com/tutorials/d3/binding-data/
-    names.forEach((name) => {
-		d3.select("#selDataset").append("option").text(name);
-    })
-    
-}));
+
+d3.json('samples.json').then((data) => {
+    console.log(data);
+    // samples= {id: "940", otu_ids: Array(80), sample_values: Array(80), otu_labels: Array(80)}
+    var samples = data.samples;
+    console.log(samples);
+
+    var sampleValues = samples.filter(item => item.id === "940")[0].sample_values;
+    console.log("Sample Values", sampleValues);
+
+    var top10samples = sampleValues.slice(0,10).reverse();
+    console.log(top10samples);
+
+    var labels = top10samples.filter(item => item.id === "940")[0].otu_ids;
+    console.log("OTU-IDS", labels);
+
+    var hover = top10samples.filter(item => item.id === "940")[0].otu_labels;
+    console.log("Hover Text", hover);
+
+
+});
