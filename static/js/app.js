6a2bf7fa99f781711c49f7644c56ec3da39c5dfb
JeniLam <jenilam639@gmail.com>
2020-12-17 23:24:02 -0600

    Gets bar chart on page

--- static/js/app.js
+++ static/js/app.js
@@ -12,14 +12,45 @@ d3.json('samples.json').then((data) => {
     var sampleValues = samples.filter(item => item.id === "940")[0].sample_values;
     console.log("Sample Values", sampleValues);
 
-    var top10samples = sampleValues.slice(0,10).reverse();
-    console.log(top10samples);
+    var top10samples = sampleValues.slice(0, 10).reverse();
+    console.log("Bar Chart Values", top10samples);
 
-    var labels = top10samples.filter(item => item.id === "940")[0].otu_ids;
+    var labels = samples.filter(item => item.id === "940")[0].otu_ids;
     console.log("OTU-IDS", labels);
 
-    var hover = top10samples.filter(item => item.id === "940")[0].otu_labels;
+    var top10Labels = labels.slice(0, 10).reverse();
+    console.log("OTU Labels", top10Labels);
+
+    var hover = samples.filter(item => item.id === "940")[0].otu_labels;
     console.log("Hover Text", hover);
 
+    var top10Hover = hover.slice(0, 10).reverse();
+    console.log("Top 10 Hover Text", top10Hover);
 
+    // build chart
+    var trace1 = {
+        type: "bar",
+        orientation: "h",
+        x: top10samples,
+        y: top10Labels,
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        xaxis: {title: "Sample Values"},
+        yaxis: {title: "out-ids"}
+    };
+
+    Plotly.newPlot("bar", data, layout);
+
+
+
+
+    // use d3 to add test subject ID to drop down (need to loop through using forEach to get all in)
+    var names = data.names;
+    // https://alignedleft.com/tutorials/d3/binding-data/
+    names.forEach((name) => {
+        d3.select("#selDataset").append("option").text(name);
+    });
 });
