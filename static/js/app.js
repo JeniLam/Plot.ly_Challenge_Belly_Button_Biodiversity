7af2fa087b3a0319a6aec272fcb914544e37127d
JeniLam <jenilam639@gmail.com>
2020-12-18 22:14:02 -0600

    Updates demographic panel

--- static/js/app.js
+++ static/js/app.js
@@ -104,7 +104,7 @@ d3.json('samples.json').then((data) => {
     panel.html("");
     Object.entries(filteredMetaData).forEach(([key, value]) => {
         var cell = panel.append("div");
-        cell.text(`${key}, ${value}`);
+        cell.text(`${key}:${value}`);
     });
 });
 
